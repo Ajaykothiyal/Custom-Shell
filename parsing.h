691a5a6dcd660d3bffb2743c34021ae563747f5d
Ajay Kothiya <145798967+Ajaykothiyal@users.noreply.github.com>
2025-06-27 00:29:03 +0530

    Add files via upload

---
+++ parsing.h
@@ -0,0 +1,10 @@
+// parsing.h - Header for command parsing functions
+
+#ifndef PARSING_H
+#define PARSING_H
+
+// Parsing functions
+void checkInfile(char *tokens);
+void checkOutfile(char *tokens);
+
+#endif
